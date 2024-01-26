adb5e93c5932b0025cf504fcdbc81247259ff9f1
Rodivanrooijen <rodivanrooijen@gmail.com>
2024-01-26 14:33:11 +0100

    15-1 eerste

--- main.py
+++ main.py
@@ -77,11 +77,11 @@ def get_db():
         db.close()
 
 def get_input_parameters(
-    stad: str = Query(..., description="The city name"),
-    checkin_datum: str = Query(..., description="Check-in date"),
-    num_volwassenen: int = Query(..., description="Number of adults"),
-    num_kinderen: int = Query(..., description="Number of children"),
-    max_paginas: int = Query(..., description="Maximum pages to scrape"),
+    stad: str = Query("Maastricht", description="The city name"),
+    checkin_datum: str = Query("2024-01-25", description="Check-in date"),
+    num_volwassenen: int = Query(2, description="Number of adults"),
+    num_kinderen: int = Query(0, description="Number of children"),
+    max_paginas: int = Query(2, description="Maximum pages to scrape"),
 ):
     return stad, checkin_datum, num_volwassenen, num_kinderen, max_paginas
 
@@ -94,7 +94,7 @@ async def scrapingresult(
     request: Request,
     input_params: tuple[str, str, int, int, int] = Depends(get_input_parameters),
 ):
-    global last_execution_time, last_execution_status, hotelgegevens
+    global last_execution_time, last_execution_status, hotelgegevens, stad, checkin_datum
 
     # Extract values from the tuple
     stad, checkin_datum, num_volwassenen, num_kinderen, max_paginas = input_params
